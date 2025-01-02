7e0f40eb7b60e2ccf65b881c5109fe884556ce6c
Gerrit Deines <Gerrit.Deines@improving.com>
2025-01-02 13:39:56 -0600

    Our first commands

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        do
+        {
+            Console.Write(" > ");
+            string input = Console.ReadLine();
+            input = input.ToLower();
+            input = input.Trim();
+            if (input == "lol") { Console.WriteLine("You Laughed out Loud!"); }
+            else if (input == "exit") { break; }
+            else { Console.WriteLine("I can't do that Hal..."); }
+        } while (true);
+    }
+}
+
