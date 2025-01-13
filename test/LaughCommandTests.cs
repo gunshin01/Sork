fd211e6a09fb97cfcdb78a8dba85d1bd40f6b4d1
Gerrit Deines <Gerrit.Deines@improving.com>
2025-01-13 14:00:12 -0600

    Lesson 5

---
+++ test/LaughCommandTests.cs
@@ -0,0 +1,16 @@
+﻿namespace Sork.Tests;
+using Sork.Commands;
+[TestClass]
+public sealed class LaughCommandsTests
+{
+    [TestMethod]
+    public void Handle_ShouldReturnTrue_WhenInputIsCapitalized()
+    {
+        //Arrange
+        var command = new LaughCommand(new UserInputOutput());
+        //Act
+        var result = command.Handles("LOL");
+        //Assert
+        Assert.IsTrue(result);
+    }
+}
